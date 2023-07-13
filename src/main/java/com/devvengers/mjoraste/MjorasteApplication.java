7e4f1fdd79010ef8f120a36e0b27db30339f71d8
Mert Kagan Aktas <mertkagan77@gmail.com>
2023-07-13 14:34:14 +0300

    backend project created.

---
+++ src/main/java/com/devvengers/mjoraste/MjorasteApplication.java
@@ -0,0 +1,17 @@
+package com.devvengers.mjoraste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableSwagger2
+public class MjorasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjorasteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devvengers/mjoraste/MjorasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvengers.mjoraste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjorasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
