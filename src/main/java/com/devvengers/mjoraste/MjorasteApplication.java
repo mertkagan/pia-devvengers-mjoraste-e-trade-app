41ddc36dfdf972448d252a85a6a94f0cf2e5e135
Mert Kagan Aktas <mertkagan77@gmail.com>
2023-07-13 15:17:46 +0300

    ModelMapper ve Swagger eklendi.

--- src/main/java/com/devvengers/mjoraste/MjorasteApplication.java
+++ src/main/java/com/devvengers/mjoraste/MjorasteApplication.java
@@ -1,9 +1,11 @@
 package com.devvengers.mjoraste;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -12,6 +14,13 @@ public class MjorasteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MjorasteApplication.class, args);
+	}
+
+
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
